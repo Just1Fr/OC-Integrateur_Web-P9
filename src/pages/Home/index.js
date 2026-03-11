5b5a8247c93fc84425a29a66e3059c5fc06155a0
Just1Fr <190003579+Just1Fr@users.noreply.github.com>
2026-03-11 18:02:08 +0100

    fix: correct various spelling errors

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -60,7 +60,7 @@ const Page = () => {
       </section>
       <section id="notre-equipe" className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
-        <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
+        <p>Une équipe d’experts dédiés à l’organisation de vos événements</p>
         <div className="ListContainer">
           <PeopleCard
             imageSrc="/images/stephanie-liverani-Zz5LQe-VSMY-unsplash.png"
@@ -118,7 +118,7 @@ const Page = () => {
     </main>
     <footer className="row">
       <div className="col presta">
-        <h3>Notre derniére prestation</h3>
+        <h3>Notre dernière prestation</h3>
         <EventCard
           imageSrc={last?.cover}
           title={last?.title}
